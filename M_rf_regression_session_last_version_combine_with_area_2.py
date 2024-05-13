69185930e63d99f69831725c9271186260ba526a
mojtabamansori <13mrlien13@gmail.com>
2024-05-13 11:37:10 +0330

    update

--- M_rf_regression_session_last_version_combine_with_area_2.py
+++ M_rf_regression_session_last_version_combine_with_area_2.py
@@ -23,12 +23,10 @@ def preproces(x, number):
 def Label_area(pre):
     pr = pre[:, 1].copy()
     for i in range(len(pr)):
-        if pr[i] < 3.94:
+        if pr[i] < 4.1:
             pr[i] = 0
-        if ((pr[i] > 3.94) & (pr[i] < 4.03)):
+        if pr[i] > 4.1:
             pr[i] = 1
-        if pr[i] > 4.03:
-            pr[i] = 2
     return pr
 
 
@@ -94,8 +92,8 @@ def evaluation1(Y_test_combined, pred, i2, number):
 
 list_1 = [9, 10, 11, 12, 17, 19, 20, 22, 26, 30, 58, 61, 66, 70, 71, 72, 75, 82, 83, 84, 85,86, 88, 89, 90, 91 ,
           92, 94, 96, 97, 99, 100, 101, 103, 104, 105, 107, 110, 118, 119, 3 , 5, 23, 25, 27, 67]
-list_2 = [28, 24, 18, 62, 102, 126, 3, 5, 23, 25, 27, 67]
-list_2 = [3, 5, 23, 25, 27, 67]
+# list_2 = [28, 24, 18, 62, 102, 126, 3, 5, 23, 25, 27, 67]
+# list_2 = [3, 5, 23, 25, 27, 67]
 list_3 = [0, 1, 2, 4, 6, 7, 8, 13, 14, 15, 16, 21, 29, 31, 32, 33, 36, 37, 38, 39, 40, 43, 44, 59, 60, 64, 68, 73, 109,3, 5, 23, 25, 27, 67]
 
 
@@ -152,22 +150,22 @@ for n_1 in range(75, 76, 5):
                 X_train_combined_all, X_test_combined_all = list_to_data(list_fualt_not,
                                                                          X_train_combined, X_test_combined)
                 X_train_combined_1, X_test_combined_1 = list_to_data(list_1, X_train_combined, X_test_combined)
-                X_train_combined_2, X_test_combined_2 = list_to_data(list_2, X_train_combined, X_test_combined)
+                # X_train_combined_2, X_test_combined_2 = list_to_data(list_2, X_train_combined, X_test_combined)
                 X_train_combined_3, X_test_combined_3 = list_to_data(list_3, X_train_combined, X_test_combined)
 
-                index_1 = (Y_train_combined[:, 1] < 3.94)
-                index_2 = ((3.94 < Y_train_combined[:, 1]) & (Y_train_combined[:, 1] < 4.03))
-                index_3 = (4.03 < Y_train_combined[:, 1])
+                index_1 = (Y_train_combined[:, 1] < 4.1)
+                # index_2 = ((3.94 < Y_train_combined[:, 1]) & (Y_train_combined[:, 1] < 4.03))
+                index_3 = (4.1 < Y_train_combined[:, 1])
 
 
             # **************************************************************
 
                 X_train_combined_1 = X_train_combined_1[index_1, :]
-                X_train_combined_2 = X_train_combined_2[index_2, :]
+                # X_train_combined_2 = X_train_combined_2[index_2, :]
                 X_train_combined_3 = X_train_combined_3[index_3, :]
 
                 Y_train_combined_1 = Y_train_combined[index_1, :]
-                Y_train_combined_2 = Y_train_combined[index_2, :]
+                # Y_train_combined_2 = Y_train_combined[index_2, :]
                 Y_train_combined_3 = Y_train_combined[index_3, :]
 
                 # Y_train_combined_1 = Y_train_combined
@@ -180,12 +178,12 @@ for n_1 in range(75, 76, 5):
                 for i_pre in [0, 1, 2]:
                     X_train_combined_p, t = preproces(X_train_combined_all, i_pre)
                     X_train_combined_p_1, t = preproces(X_train_combined_1, i_pre)
-                    X_train_combined_p_2, t = preproces(X_train_combined_2, i_pre)
+                    # X_train_combined_p_2, t = preproces(X_train_combined_2, i_pre)
                     X_train_combined_p_3, t = preproces(X_train_combined_3, i_pre)
 
                     X_test_combined_p, t = preproces(X_test_combined_all, i_pre)
                     X_test_combined_p_1, t = preproces(X_test_combined_1, i_pre)
-                    X_test_combined_p_2, t = preproces(X_test_combined_2, i_pre)
+                    # X_test_combined_p_2, t = preproces(X_test_combined_2, i_pre)
                     X_test_combined_p_3, t = preproces(X_test_combined_3, i_pre)
 
 
@@ -199,14 +197,14 @@ for n_1 in range(75, 76, 5):
                     regressor_1.fit(X_train_combined_p_1, Y_train_combined_1)
                     # print('model 1 done')
                     # print('model 2')
-                    regressor_2.fit(X_train_combined_p_2, Y_train_combined_2)
+                    # regressor_2.fit(X_train_combined_p_2, Y_train_combined_2)
                     # print('model 2 done')
                     # print('model 3')
                     regressor_3.fit(X_train_combined_p_3, Y_train_combined_3)
                     # print('model 3 done')
 
                     pred_1 = regressor_1.predict(X_test_combined_p_1)
-                    pred_2 = regressor_2.predict(X_test_combined_p_2)
+                    # pred_2 = regressor_2.predict(X_test_combined_p_2)
                     pred_3 = regressor_3.predict(X_test_combined_p_3)
 
                     print('model************************')
@@ -215,21 +213,21 @@ for n_1 in range(75, 76, 5):
                     labe_are = Label_area(pred)
 
                     pred_1_2 = pred_1[labe_are == 0]
-                    pred_2_2 = pred_2[labe_are == 1]
+                    # pred_2_2 = pred_2[labe_are == 1]
                     pred_3_2 = pred_3[labe_are == 2]
                     Y_test_combined_1 = Y_test_combined[labe_are == 0]
-                    Y_test_combined_2 = Y_test_combined[labe_are == 1]
+                    # Y_test_combined_2 = Y_test_combined[labe_are == 1]
                     Y_test_combined_3 = Y_test_combined[labe_are == 2]
                     evaluation1(Y_test_combined_1, pred_1_2, random_seed_number, i_pre)
-                    evaluation1(Y_test_combined_2, pred_2_2, random_seed_number, i_pre)
+                    # evaluation1(Y_test_combined_2, pred_2_2, random_seed_number, i_pre)
                     evaluation1(Y_test_combined_3, pred_3_2, random_seed_number, i_pre)
 
                     for i_number_label in range(len(X_test_combined)):
                         if labe_are[i_number_label] == 0:
                             pred[i_number_label, :] = pred_1[i_number_label, :]
+                        # if labe_are[i_number_label] == 1:
+                        #     pred[i_number_label, :] = pred_2[i_number_label, :]
                         if labe_are[i_number_label] == 1:
-                            pred[i_number_label, :] = pred_2[i_number_label, :]
-                        if labe_are[i_number_label] == 2:
                             pred[i_number_label, :] = pred_3[i_number_label, :]
 
                     print('model 3                      model all')
